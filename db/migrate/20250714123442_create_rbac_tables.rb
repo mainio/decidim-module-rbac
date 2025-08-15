92430f22cddeab9b0d9549ef780b69e3a4fc86b0
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-08-15 16:31:24 +0300

    Fix the table name in the migration in plural format

--- db/migrate/20250714123442_create_rbac_tables.rb
+++ db/migrate/20250714123442_create_rbac_tables.rb
@@ -15,7 +15,7 @@ class CreateRbacTables < ActiveRecord::Migration[6.1]
     #
     # The `resource` is the type of the resouce represented as a string, e.g.
     # "organization".
-    create_table :decidim_rbac_permission_role_assignment do |t|
+    create_table :decidim_rbac_permission_role_assignments do |t|
       t.string :role, null: false
       t.references :record, null: false, polymorphic: true, index: { name: "index_permission_role_assignment_on_record" }
       t.references :subject, polymorphic: true, index: { name: "index_permission_role_assignment_on_subject" }
