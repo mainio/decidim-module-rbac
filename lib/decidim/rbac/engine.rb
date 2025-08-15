6adfbd1880a3e7a3dd02379773e594baebcc8c1c
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-08-15 16:34:29 +0300

    Automatically load the `permission.rb` config files from modules and app

--- lib/decidim/rbac/engine.rb
+++ lib/decidim/rbac/engine.rb
@@ -23,6 +23,24 @@ module Decidim
       end
       # rubocop:enable Lint/UselessAssignment
 
+      initializer "decidim_rbac.permissions" do
+        next unless defined?(Rails)
+        next unless Rails.application
+
+        # Permissions from the modules
+        decidim_gems = Bundler.load.specs.select { |spec| spec.name =~ /^decidim-/ }
+        decidim_gems.each do |gem|
+          asset_config_path = File.join(gem.full_gem_path, "config/permissions.rb")
+          next unless File.exist?(asset_config_path)
+
+          load asset_config_path
+        end
+
+        # Application permissions
+        asset_config_path = File.join(Rails.application.root, "config/permissions.rb")
+        load asset_config_path if File.exist?(asset_config_path)
+      end
+
       initializer "decidim_rbac.customizations" do
         config.to_prepare do
           # Models
