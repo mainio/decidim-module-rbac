b041c5eca12feab55139b5de83b3010df0ab258e
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2025-08-21 15:37:06 +0300

    Make assembly_admin permission show only permitted spaces

--- lib/decidim/rbac/policy/space_area.rb
+++ lib/decidim/rbac/policy/space_area.rb
@@ -8,8 +8,27 @@ module Decidim
 
         def able?(operation)
           return false unless subject
+          return false unless operation == :admin_enter
 
-          operation == :admin_enter
+          able_to_enter_space?
+        end
+
+        private
+
+        def able_to_enter_space?
+          record_types = subject.permission_role_assignments.pluck(:record_type)
+
+          record_types.include?(space_types[space_name])
+        end
+
+        def space_types
+          {
+            assemblies: "Decidim::Assembly",
+            conferences: "Decidim::Conference",
+            initiatives: "Decidim::Initiative",
+            processes: "Decidim::ParticipatoryProcess",
+            process_groups: "Decidim::ParticipatoryProcessGroup"
+          }
         end
       end
     end
