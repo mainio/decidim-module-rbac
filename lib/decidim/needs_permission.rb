8b3c68e1f638b4c0dc2b9517325c29f170f1e42e
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2025-08-28 11:19:36 +0300

    Fix lazy loading of contexts

--- lib/decidim/needs_permission.rb
+++ lib/decidim/needs_permission.rb
@@ -52,13 +52,12 @@ module Decidim
           Rails.logger.debug [permission_scope, operation, permission, permission_class_chain].map(&:inspect).join("\n")
           Rails.logger.debug "==========="
         end
-
+        
         # allowed_to?(operation, permission, extra_context, permission_class_chain, current_user, permission_scope)
         unless allowed_to?(operation, permission, extra_context, permission_class_chain, current_user, permission_scope)
           record = extra_context[:record] || extra_context[:resource]
           record ||= extra_context[permission] if permission.is_a?(Symbol)
           record ||= extra_context[:trashable_deleted_resource] if extra_context.has_key?(:trashable_deleted_resource) && [:restore, :soft_delete, :read].include?(operation)
-
           within = record || try(:current_component) || try(:current_participatory_space) || try(:current_organization)
 
           raise Decidim::ActionForbidden,
@@ -81,8 +80,15 @@ module Decidim
         record = extra_context[:record] || extra_context[:resource]
         record ||= extra_context[resource] if resource.is_a?(Symbol)
         record ||= extra_context[:trashable_deleted_resource] if extra_context.has_key?(:trashable_deleted_resource) && [:restore, :soft_delete].include?(operation)
+        # Try would not necessarily return the expected result, since some of the methods that are being called are lazier than others
+        # (i.e current_participatory_space compared to current organization).
+        # within = record || try(:current_component) || try(:current_participatory_space) || try(:current_organization)
+        within = record ||
+          permissions_context[:current_participatory_process] ||
+          permissions_context[:current_component] ||
+          permissions_context[:current_participatory_space] ||
+          permissions_context[:current_organization]
 
-        within = record || try(:current_component) || try(:current_participatory_space) || try(:current_organization)
         policy = RBAC.policy(resource).new(
           record: record,
           subject: subject,
@@ -93,7 +99,7 @@ module Decidim
         # Ideally in the permission checks, we should already check against the
         # admin operations instead of providing the separate scope.
         if scope == :admin || permission_scope == :admin
-         
+          if policy.class.name == "Decidim::RBAC::Policy::Component"
             puts "-----------" * 100
             puts "admin_#{operation}"
             puts "POLICY #{policy.class.name}"
@@ -102,6 +108,7 @@ module Decidim
             puts "subject: #{subject.id}"
             puts policy.apply(:"admin_#{operation}").inspect
             puts "-----------"
+          end
           
           policy.apply(:"admin_#{operation}")
         else
