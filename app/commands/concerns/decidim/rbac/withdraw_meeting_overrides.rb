06da5872bfdc81590eaf798e1f4eca185037fbe7
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2025-09-30 10:10:54 +0300

    Unassign meeting author role on destroying a meeting

--- app/commands/concerns/decidim/rbac/withdraw_meeting_overrides.rb
+++ app/commands/concerns/decidim/rbac/withdraw_meeting_overrides.rb
@@ -6,9 +6,15 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
-        transaction do
-          change_meeting_state_to_withdrawn
-          @current_user.unassign_role!("meeting_author", @meeting)
+        def call
+          return broadcast(:invalid) unless @meeting.authored_by?(@current_user)
+
+          transaction do
+            change_meeting_state_to_withdrawn
+            @current_user.unassign_role!("meeting_author", @meeting)
+          end
+
+          broadcast(:ok, @meeting)
         end
       end
     end
